7634df72c43b84a4cfbb80a2f3e90253a3cec65a
Ruby Adkins <radkins22@students.stab.org>
2021-09-11 23:24:33 -0400

    handle checking gesture on event

--- src/App.js
+++ src/App.js
@@ -4,95 +4,119 @@ import * as tf from "@tensorflow/tfjs";
 import * as handpose from "@tensorflow-models/handpose";
 import Webcam from "react-webcam";
 import './App.css';
-import {drawHand} from "./utilities";
+import { drawHand } from "./utilities";
 
 //import stuff for gesture recognition
 import * as fp from "fingerpose";
+
+//import emoji images
 import thumbs_up from "./gestureEmojis/thumbs_up.png";
 import scissors from "./gestureEmojis/scissors.jpg"
 import paper from "./gestureEmojis/paper.png";
 import rock from "./gestureEmojis/rock.png";
 
 //import custom gestures
-import {PaperGesture} from "./customGestures/paper";
-import {RockGesture} from "./customGestures/rock";
-import {ScissorsGesture} from "./customGestures/scissors";
+import { PaperGesture } from "./customGestures/paper";
+import { RockGesture } from "./customGestures/rock";
+import { ScissorsGesture } from "./customGestures/scissors";
+
+//boolean to determine if check gesture
+let checkGesture = false;
 
 function App() {
   //define ref
   const webcamRef = useRef(null);
   const canvasRef = useRef(null);
 
+  const FunctionClick = ()=> {
+    function clickHandler() {
+      checkGesture =! checkGesture;
+    }
+
+    return (
+        <div>
+            <button onClick={clickHandler}>Click</button>
+        </div>
+    )
+}
+
+
   //can use states to connect gesture with emoji
   const [emoji, setEmoji] = useState(null);
   //for displaying purposes
-  const images = {thumbs_up:thumbs_up, paper:paper, rock:rock, scissors:scissors};
+  const images = { thumbs_up: thumbs_up, paper: paper, rock: rock, scissors: scissors };
 
   //load handpose
-  const runHandpose = async() =>{
+  const runHandpose = async () => {
     const NeuralNet = await handpose.load();
-    
+
     //loop and detect hands
-  setInterval(()=>{
-    detect(NeuralNet);
-  }, 100)
+    setInterval(() => {
+      detect(NeuralNet);
+    }, 100)
   }
 
-  const detect = async(NeuralNet) => {
+  const detect = async (NeuralNet) => {
     //check if data is availible
     if (typeof webcamRef.current !== "undefined" && //check if webcam undefined
-    webcamRef.current !== null && //check webcam null
-    webcamRef.current.video.readyState === 4 //check videocam readystate
+      webcamRef.current !== null && //check webcam null
+      webcamRef.current.video.readyState === 4 //check videocam readystate
     ) {
-    //get video data
-    const video = webcamRef.current.video;
-    const videoWidth = video.videoWidth;
-    const videoHeight =  video.videoHeight;
+      //get video data
+      const video = webcamRef.current.video;
+      const videoWidth = video.videoWidth;
+      const videoHeight = video.videoHeight;
 
-    //set video height/width
-    webcamRef.current.video.width = videoWidth;
-    webcamRef.current.video.height = videoHeight;
+      //set video height/width
+      webcamRef.current.video.width = videoWidth;
+      webcamRef.current.video.height = videoHeight;
 
-    //set canvas height/width to same as video
-    canvasRef.current.width = videoWidth;
-    canvasRef.current.height = videoHeight;
+      //set canvas height/width to same as video
+      canvasRef.current.width = videoWidth;
+      canvasRef.current.height = videoHeight;
 
-    //make detection
-    const hand = await NeuralNet.estimateHands(video);
+      //make detection
+      const hand = await NeuralNet.estimateHands(video);
 
-    if (hand.length >0) {
-      //import gestures
-      const GE = new fp.GestureEstimator([
-        fp.Gestures.ThumbsUpGesture,
-        PaperGesture,
-        RockGesture,
-        ScissorsGesture
-      ]);
+      if (hand.length > 0) {
+        //import gestures
+        const GE = new fp.GestureEstimator([
+          fp.Gestures.ThumbsUpGesture,
+          PaperGesture,
+          RockGesture,
+          ScissorsGesture
+        ]);
 
-      //estimate  gesture by sending hand and min confidence level
-      const gesture = await GE.estimate(hand[0].landmarks, 4);
-      if (gesture.gestures !== undefined && gesture.gestures.length > 0) { //ensure gesture was detected
-        //console.log(gesture.gestures)
-        
-        //mapping confidence array
-        const confidence = gesture.gestures.map(
-          (prediction) => prediction.confidence
-        );
+        //estimate  gesture by sending hand and min confidence level
+        const gesture = await GE.estimate(hand[0].landmarks, 4);
+        if (gesture.gestures !== undefined && gesture.gestures.length > 0) { //ensure gesture was detected
+         // console.log(gesture.gestures)
 
-        //grab max confidence index from the array
-        const maxConfidence = confidence.indexOf(
-          Math.max.apply(null, confidence)
-        )
+          //mapping confidence array
+          const confidence = gesture.gestures.map(
+            (prediction) => prediction.confidence
+          );
 
-        //set state
-        setEmoji(gesture.gestures[maxConfidence].name);
+          //grab max confidence index from the array
+          const maxConfidence = confidence.indexOf(
+            Math.max.apply(null, confidence)
+          )
+
+          //get gesture
+          console.log(checkGesture)
+          if (checkGesture) {
+            console.log(gesture.gestures[maxConfidence].name);
+          }
+
+          //set state
+          setEmoji(gesture.gestures[maxConfidence].name);
+        }
+
       }
 
-    }
-
-    //draw hand mesh
-    const ctx = canvasRef.current.getContext("2d");
-    drawHand(hand, ctx);
+      //draw hand mesh
+      const ctx = canvasRef.current.getContext("2d");
+      drawHand(hand, ctx);
     }
   }
 
@@ -100,6 +124,7 @@ function App() {
 
   return (
     <div className="App">
+       <FunctionClick></FunctionClick>
       <header className="App-header">
         <Webcam ref={webcamRef}
           style={{
@@ -125,16 +150,16 @@ function App() {
             width: 648,
             height: 480
           }} />
-          {emoji !==null ? <img src={images[emoji]} alt="gesture" style={{
-            position: "absolute",
-            marginLeft: "auto",
-            marginRight: "auto",
-            left: 400,
-            bottom: 500,
-            right: 0,
-            textAlign: "center",
-            height: 100
-          }} />: ""}
+        {emoji !== null ? <img src={images[emoji]} alt="gesture" style={{
+          position: "absolute",
+          marginLeft: "auto",
+          marginRight: "auto",
+          left: 400,
+          bottom: 500,
+          right: 0,
+          textAlign: "center",
+          height: 100
+        }} /> : ""}
       </header>
     </div>
   );
